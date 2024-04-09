7483613446aaec0f8b0fb0b9e4b1a4cbe2cc9e57
YUKUN-ZHANG1 <137257662+YUKUN-ZHANG1@users.noreply.github.com>
2024-04-09 11:10:28 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -28,12 +28,43 @@ const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_P
 const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
 const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
+const STAR_PLATINUM = "https://static.jojowiki.com/images/thumb/c/ca/latest/20200927002609/Star_Platinum_SC_Infobox_Anime.png/800px-Star_Platinum_SC_Infobox_Anime.png";
+const MAGICIANS_RED = "https://static.jojowiki.com/images/4/48/latest/20220916201225/Magician%27s_Red_Infobox_Anime.png";
+const HERMIT_PURPLE = "https://static.jojowiki.com/images/c/c1/latest/20191015212906/Hermit_Purple_SC_Infobox_Manga.png";
+const HIEROPHANT_GREEN = "https://static.jojowiki.com/images/thumb/3/3e/latest/20210616200106/Hierophant_Green_Infobox_Anime.png/800px-Hierophant_Green_Infobox_Anime.png";
+const SILVER_CHARIOT = "https://static.jojowiki.com/images/9/9b/latest/20220502142632/Silver_Chariot_SC_Infobox_Manga.png";
+const THE_FOOL = "https://static.jojowiki.com/images/thumb/1/10/latest/20210312225357/The_Fool_Infobox_Anime.png/800px-The_Fool_Infobox_Anime.png";
+const THE_WORLD = "https://static.jojowiki.com/images/7/7e/latest/20221004175724/The_World_Infobox_Anime.png";
+const CRAZY_DIAMOND = "https://static.jojowiki.com/images/b/b5/latest/20191015215419/Crazy_Diamond_Infobox_Anime.png";
+const THE_HAND = "https://static.jojowiki.com/images/thumb/a/ae/latest/20210111150120/The_Hand_Infobox_Anime.png/800px-The_Hand_Infobox_Anime.png";
+const ECHOES_ACT1 = "https://static.jojowiki.com/images/d/dc/latest/20210424210829/Echoes_ACT1_DU_Infobox_Anime.png";
+const ECHOES_ACT2 = "https://static.jojowiki.com/images/thumb/6/69/latest/20231215131846/Echoes_ACT2_DU_Infobox_Anime.png/800px-Echoes_ACT2_DU_Infobox_Anime.png";
+const ECHOES_ACT3 = "https://static.jojowiki.com/images/8/8e/latest/20210422175130/Echoes_ACT3_DU_Infobox_Anime.png";
+const HEAVENS_DOOR = "https://static.jojowiki.com/images/5/50/latest/20210203193008/Heaven%27s_Door_Infobox_Anime.png";
+const KILLER_QUEEN = "https://static.jojowiki.com/images/f/fb/latest/20210420222949/Killer_Queen_Infobox_Manga.png";
+const SHEER_HEART_ATTACK = "https://static.jojowiki.com/images/f/fc/latest/20191015215748/Sheer_Heart_Attack_Infobox_Manga.png";
+const BITES_THE_DUST = "https://static.jojowiki.com/images/f/f9/latest/20191015213645/Bites_the_Dust_Infobox_Manga.png";
+const GOLD_EXPERIENCE = "https://static.jojowiki.com/images/8/81/latest/20210707053105/Gold_Experience_Infobox_Anime.png";
+const GOLD_EXPERIENCE_REQUIEM = "https://static.jojowiki.com/images/1/19/latest/20220802141706/GER_Infobox_Anime.png";
+const STICKY_FINGERS = "https://static.jojowiki.com/images/7/78/latest/20221006032155/Sticky_Fingers_Infobox_Anime.png";
+const MOODY_BLUES = "https://static.jojowiki.com/images/9/91/latest/20220916205647/Moody_Blues_Infobox_Anime.png";
+const SEX_PISTOLS = "https://static.jojowiki.com/images/b/b9/latest/20210521114106/Sex_Pistols_Infobox_Anime.png";
+const AEROSMITH = "https://static.jojowiki.com/images/7/7c/latest/20210701024635/Aerosmith_Infobox_Anime.png";
+const PURPLE_HAZE = "https://static.jojowiki.com/images/c/ca/latest/20191015214140/Purple_Haze_Infobox_Anime.png";
+const SPICE_GIRL = "https://static.jojowiki.com/images/2/21/latest/20200118002424/Spice_Girl_Infobox_Anime.png";
+const MR_PRESIDENT = "https://static.jojowiki.com/images/9/9e/latest/20191015214038/Mr.President_Infobox_Manga.png";
+const CHARIOT_REQUIEM = "https://static.jojowiki.com/images/1/12/latest/20211210020712/Chariot_Requiem_Infobox_Anime.png";
+const KING_CRIMSON = "https://static.jojowiki.com/images/thumb/c/c6/latest/20191015213856/King_Crimson_Infobox_Anime.png/800px-King_Crimson_Infobox_Anime.png";
+
+
 // This is an array of strings (TV show titles)
 let titles = [
     "Fresh Prince of Bel Air",
     "Curb Your Enthusiasm",
     "East Los High"
 ];
+
+let 
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
 
